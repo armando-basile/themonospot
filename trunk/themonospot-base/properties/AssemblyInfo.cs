542c32638236362366e9d1a36bbf1cec0a42c9f4
hmandevteam <hmandevteam@1f500557-aa2b-0410-ab1c-2dafa0fca65b>
2009-12-13 14:31:04 +0000

    * themonospot-base.csproj: * properties/AssemblyInfo.cs:
    - added sign to project file and removed from AssemblyInfo

--- trunk/themonospot-base/properties/AssemblyInfo.cs
+++ trunk/themonospot-base/properties/AssemblyInfo.cs
@@ -11,6 +11,6 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyTrademark("None")]
 [assembly: AssemblyDelaySign(false)]
 
-[assembly: AssemblyKeyFile("themonospot-base.snk")]
+//[assembly: AssemblyKeyFile("themonospot-base.snk")]
 
 
