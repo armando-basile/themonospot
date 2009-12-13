e301ee887c951fe2860390ed7cb10808a472060b
hmandevteam <hmandevteam@1f500557-aa2b-0410-ab1c-2dafa0fca65b>
2009-12-13 14:26:14 +0000

    * properties/AssemblyInfo.cs: * themonospot-plugin-manager.csproj:
    - added sign to project file and removed from AssemblyInfo

--- trunk/themonospot-plugin-manager/properties/AssemblyInfo.cs
+++ trunk/themonospot-plugin-manager/properties/AssemblyInfo.cs
@@ -23,4 +23,4 @@ using System.Runtime.CompilerServices;
 // if desired. See the Mono documentation for more information about signing.
 
 [assembly: AssemblyDelaySign(false)]
-[assembly: AssemblyKeyFile("themonospot-plugin-manager.snk")]
+//[assembly: AssemblyKeyFile("themonospot-plugin-manager.snk")]
